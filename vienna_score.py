d7947c876185356b0843f65a44d4c0479f4eb1dc
sreenieadara <sreeni.eadara@gmail.com>
2023-08-11 13:15:23 -0500

    testing vienna score. C is not in use right now

--- vienna_score.py
+++ vienna_score.py
@@ -1,4 +1,3 @@
-#from ctypes import *
 import RNA
 from math import exp, log10
 from params import design_parameters
@@ -7,7 +6,7 @@ def vienna_score(sequence:str, score_region:list, design_parameters:design_param
     if len(sequence) != len(score_region):
         raise ValueError
     
-        #Calculate cold temp for scoring
+    #Calculate cold temp for scoring
     cold_temp = design_parameters.target_temp-design_parameters.temp_offset
     if cold_temp < 0:
         cold_temp = 0
@@ -21,8 +20,6 @@ def vienna_score(sequence:str, score_region:list, design_parameters:design_param
     if hot_temp > 100:
         hot_temp = 100
 
-    #seq_c = c_char_p(bytes('GATTACA', 'ascii')) # Operating systems should understand ASCII
-    
     score_region_byte_list = score_region
     for i in range(len(score_region_byte_list)):
         if score_region_byte_list[i]:
@@ -30,24 +27,18 @@ def vienna_score(sequence:str, score_region:list, design_parameters:design_param
         else:
             score_region_byte_list[i] = '0'
     
-    #score_region_bytes_as_str = ''.join(score_region_byte_list)
-    #score_reg_c = c_char_p(bytes(score_region_bytes_as_str, 'ascii'))
-
-    vienna_score_temp(seq=sequence, score_region=score_region, temp=hot_temp,
+    scores_hot = vienna_score_temp(seq=sequence, score_region=score_region, temp=hot_temp,
         max_dimer_monomer_factor=design_parameters.max_dimer_monomer_factor,
         nucl_max_score=design_parameters.nucl_max_score)
-    vienna_score_temp(seq=sequence, score_region=score_region, temp=cold_temp,
+    scores_cold = vienna_score_temp(seq=sequence, score_region=score_region, temp=cold_temp,
         max_dimer_monomer_factor=design_parameters.max_dimer_monomer_factor,
         nucl_max_score=design_parameters.nucl_max_score)
 
-    vienna_dimer_energy(seq=sequence, temp=hot_temp)
-    vienna_dimer_energy(seq=sequence, temp=cold_temp)
+    score_energy = vienna_score_energy(seq=sequence, temp=design_parameters.thermo_score_temp,
+        target_energy=design_parameters.target_energy,
+        free_energy_max_score=design_parameters.free_energy_max_score)
 
-    vienna_score_energy(seq=sequence, temp=design_parameters.thermo_score_temp, target_energy=design_parameters.target_energy)
-    #vienna_score_functions = CDLL("vienna_score.so")
-    # Compilation - gcc -pthread -I/usr/local/include -I/usr/local/include/ViennaRNA -L/usr/local/lib -lRNA -lpthread -lmpfr -lgmp -lstdc++ -fPIC -shared -o vienna_score.so vienna_score.c
-    # For non-shared, gcc -pthread -I/usr/local/include -I/usr/local/include/ViennaRNA -L/usr/local/lib -lRNA -lpthread -lmpfr -lgmp -lstdc++ -o vienna_score.o vienna_score.c
-    #vienna_score_functions.score(seq_c, score_reg_c)
+    return score_energy + sum(scores_hot) + sum(scores_cold)
 
 # Returns (float: score_nucl, float: ensemble_energy)
 def vienna_score_temp(seq:str, score_region:list, temp: float, max_dimer_monomer_factor: float, nucl_max_score: float) -> tuple[float, float]:
@@ -66,7 +57,6 @@ def vienna_score_temp(seq:str, score_region:list, temp: float, max_dimer_monomer
             #Perhaps this can be sped up by copying fc.bpp() to another structure?
             #May be unnecessary. TODO consider changing that
             basepair_probs[i - 1] += fc.bpp()[i][j] + fc.bpp()[j][i]
-
 
     if len(basepair_probs) != len(score_region):
         raise ValueError
@@ -108,25 +98,26 @@ def vienna_dimer_energy(seq:str, temp:float) -> float:
     (ss, dimer_energy) = fc.pf()
     return dimer_energy
 
-def vienna_score_energy(seq:str, temp:float, target_energy: float) -> float:
+def vienna_score_energy(seq:str, temp:float, target_energy: float, free_energy_max_score: float) -> float:
     model = RNA.md()
     model.temperature = temp
     fc = RNA.fold_compound(seq, model)
     (ss, ensemble_energy) = fc.pf()
 
-    #TODO finish
-    return 0.0
+    score_free_energy = (target_energy - ensemble_energy) / target_energy
+    if score_free_energy < 0:
+        score_free_energy = 0
+    if score_free_energy > free_energy_max_score:
+        score_free_energy = free_energy_max_score
+    return score_free_energy
 
 # THIS IS FOR TESTING!! TODO get rid of this lmao
 from blist import blacklist
 def main():
     blist = blacklist(path="blacklist.fasta")
-    vienna_score(sequence="CCCCCAAAAAGGGGG",
+    score = vienna_score(sequence="CCCCCAAAAAGGGGG",
         score_region=[1]*5+[0]*5+[1]*5, design_parameters=design_parameters(blacklist=blist, target_temp=60,
         temp_offset=5, program="VIENNA", weights=[8, 8, 8, 8, 10, 10, 16], weight_factor=1, num_mutants=8,
         target_energy=-12.0, free_energy_max_score=1.0 , nucl_max_score=1.0, max_dimer_monomer_factor=1.0))
-    #vienna_score(sequence="GAATTC",
-    #   score_region=[1]*6, design_parameters=design_parameters(blacklist=blist, target_temp=60,
-    #   temp_offset=5, program="VIENNA", weights=[8, 8, 8, 8, 10, 10, 16], weight_factor=1, num_mutants=8,
-    #   target_energy=-12.0, free_energy_max_score=1.0 , nucl_max_score=1.0, max_dimer_monomer_factor=1.0))
+    print(score)
 main()
