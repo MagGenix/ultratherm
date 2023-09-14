7a40971b8bbbf1ffecedb228cead417756ec8bb6
sreenieadara <sreeni.eadara@gmail.com>
2023-09-14 11:12:21 -0500

    hybrid scoring WIP

--- nupack_score_hybrid.py
+++ nupack_score_hybrid.py
@@ -1,6 +1,7 @@
 from nupack import Tube, Complex, complex_analysis, complex_concentrations, Strand, SetSpec, Model
 from math import log10
 from params import design_parameters
+import numpy
 
 def nupack_score_hybrid(sequence_1:str, score_region_1:list, is_rna_1:bool, sequence_2:str, score_region_2:list, is_rna_2:bool, design_parameters:design_parameters) -> float:
     if len(sequence_1) != len(score_region_1) or len(sequence_2) != len(score_region_2):
@@ -51,15 +52,21 @@ def nupack_score_hybrid(sequence_1:str, score_region_1:list, is_rna_1:bool, sequ
         material=material, temp=cold_temp, tube_nucl=tube_nucl,
         unbound_complexes=unbound_complexes, parasitic_complexes=parasitic_complexes,
         hybrid_complex=complex_AB,
-        hot=False, parasitic_max_order_magnitude=design_parameters.dimer_max_order_magnitude)
+        hot=False, parasitic_max_order_magnitude=design_parameters.dimer_max_order_magnitude,
+        score_region_1=score_region_1, score_region_2=score_region_2)
+
+    # TODO add the rest of the scoring (hot temp, energy)
 
     return sum(scores_cold)
 def nupack_score_temp(
         material: str, temp:float, tube_nucl: Tube,
         unbound_complexes: list[Complex], parasitic_complexes: list[Complex], hybrid_complex: Complex,
-        hot: bool, parasitic_max_order_magnitude:float
+        hot: bool, parasitic_max_order_magnitude:float, score_region_1:list, score_region_2:list
 ) -> tuple[float, float, float]:
     
+    if len(hybrid_complex.strands[0]) != len(score_region_1) or len(hybrid_complex.strands[1]) != len(score_region_2):
+        raise ValueError
+
     model_nucl = Model(kelvin=temp + 273.15, material=material)
     results_nucl = complex_analysis(complexes=tube_nucl, model=model_nucl, compute=['pairs'])
     concentrations_nucl = complex_concentrations(tube=tube_nucl, data = results_nucl)
@@ -96,15 +103,37 @@ def nupack_score_temp(
             total_unbound_concentration / hybrid_concentration
         ) + parasitic_max_order_magnitude # TODO Need sep. variable!
 
-    total_bound = 0.0
-    count_scored_nuc = 0
-    diagonal = results_nucl.complexes[hybrid_complex].pairs.diagonal
 
-    # TODO finish implementation
+    pairs_arr = results_nucl.complexes[hybrid_complex].pairs.to_array()
+    
+    sub_pairs_arr = pairs_arr[0:len(score_region_1), len(score_region_1):]
+    paired_strand_1 = numpy.sum(sub_pairs_arr, axis=0)
+    paired_strand_2 = numpy.sum(sub_pairs_arr, axis=1)
+    
+    total_bound_1 = 0.0
+    count_scored_nuc_1 = 0
+
+    total_bound_2 = 0.0
+    count_scored_nuc_2 = 0
+
+    if True: # TODO replace with if score strand_1
+        for i, x in enumerate(score_region_1):
+            if x:
+                total_bound_1 += paired_strand_1[i]
+                count_scored_nuc_1+=1
+    if True: # TODO replace with if score strand_2
+        for i, x in enumerate(score_region_2):
+            if x:
+                total_bound_2 += paired_strand_2[i]
+                count_scored_nuc_2+=1
+    
+    accessibility_score = (total_bound_1 + total_bound_2) / float(count_scored_nuc_1 + count_scored_nuc_2)
+
+    if hot:
+        accessibility_score = 1.0 - accessibility_score # TODO replace 1.0 with ascribed maximum
+        hybrid_score = 1.0 - hybrid_score # TODO replace 1.0 with ascribed maximum
+
     # TODO add caps for score penalties
+    # TODO specify which strands are to be scored in the nucl_hybrid
 
-
-    return(parasitic_score, hybrid_score, accessibility_score)
-    
-
-    
+    return(parasitic_score, hybrid_score, accessibility_score)    
